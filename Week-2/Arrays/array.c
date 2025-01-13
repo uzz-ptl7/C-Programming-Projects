5d4cf110a20e9e1e99d771e11a18bd377b7656f0
uzz-ptl7 <uzairpatel7117@gmail.com>
2025-01-13 20:10:12 +0200

    Week 2 Intro with Arrays in C

---
+++ Week-2/Arrays/array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num[5] = {10, 20, 30, 40, 50};
+
+    printf("Element 1: %d\n", num[0]);
+    printf("Element 2: %d\n", num[1]);
+    printf("Element 3: %d\n", num[2]);
+    printf("Element 4: %d\n", num[3]);
+    printf("Element 5: %d\n", num[4]);
+
+    getchar();
+    return 0;
+}
