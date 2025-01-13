7aa867805b29775947eb1e1220a62ed78d1d0dad
uzz-ptl7 <uzairpatel7117@gmail.com>
2025-01-13 20:33:28 +0200

    Week 2 whileLoop in C

---
+++ Week-2/whileLoop/whileLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 1;
+
+    while (i <= 10) {
+        printf("I am a Dreamer\n");
+        i++;
+    }
+
+    getchar();
+    return 0;
+}
